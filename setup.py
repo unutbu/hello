ead9ff61e581cf5e9f32bb3af05190a836b9de20
unutbu <unutbu@gmail.com>
2010-10-18 22:28:48 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = 'hello',
+    version = '0.1',
+    description = 'An example of an installable program',
+    author = 'unutbu',
+    url = '',
+    license = 'MIT',
+    py_modules = ['hello'],
+)
